a66e0ad8e665d7893d0c9e8586fb66b5c4334727
Sudheesh <sudheeshs.6652@gmail.com>
2024-01-02 23:27:41 +0000

    update provider

--- terragrunt/dev/instance1/provider.tf
+++ terragrunt/dev/instance1/provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.31.0"
+    }
+  }
+}
+
 provider "aws" {
-  region = "ap-south-1"  # Set your preferred AWS region
+  # Configuration options
 }
