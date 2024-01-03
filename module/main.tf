22f32df90a975fc86473333befe8f177a00d6a3d
Sudheesh <sudheeshs.6652@gmail.com>
2024-01-03 13:23:20 +0000

    remove module

--- module/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "example_instance" {
-  ami           = "ami-0a0f1259dd1c90938"
-  instance_type = "t2.micro"               
-
-  tags = {
-    Name = var.instance_name
-  }
-}
